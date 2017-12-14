a4e51c2da65a91cb5951be0bc4b060c33ac073b2
mjOrtuzar <re4ortuzar@hotmail.com>
2017-12-14 12:26:26 -0300

    foodmap sin filtro

---
+++ assets/app/data.js
@@ -0,0 +1,8 @@
+var restaurantes={
+    'comida asiatica':['china ahora', 'sushi stop', 'thai food', 'sabores de india'];
+    'comida rapida':['WacDonals', 'Burger Queen', 'Ohio fried Chicken' , 'moggis'];
+    'comida italiana':['nostra Italia','piccola roma', 'nostro piacere' ];
+    'comida mexicana':['ste men','tequila con limon', 'Tacos al Pastor','Guadalaja con guacamole','nel perro', 'orale :v'];
+    'comida argentina':['pequeña argentina', 'choco Patagonia', 'a lo Carlos Gardel','soza vs gardel'];
+    'comida chilena':['Fuente Germana', 'Mote con yapa', 'Sabores del Sur', 'delicias Nortinas'];
+};
---
+++ assets/app/query.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	setTimeout(function(){
+		$('#splash').fadeOut(500);
+	}, 2000);
+
+
+});
